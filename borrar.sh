f2637d8fef966c9b945debbe0656bce326a84471
amacalli05 <sanchezamacalli@gmail.com>
2025-03-01 20:05:08 -0600

    Add files via upload
    Se sube el script de borrado y se adjunta su documentación

---
+++ borrar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+archivo=$1
+directorio="/home/amacalli/.local/share/Trash/files"
+echo "Hola, este script es para borrar un archivo"
+mv "$archivo" "$directorio"
+echo "Archivo eliminado"
+ls /home/amacalli/Descargas
