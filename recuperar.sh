e5989d4710139ce609dbc23d20893e4db01a81e5
amacalli05 <sanchezamacalli@gmail.com>
2025-03-01 20:50:00 -0600

    Add files via upload
    Se sube el script de recuperado de un archivo y se adjunta su documentación

---
+++ recuperar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+archivorec=$1
+directorio=$2
+echo "Este script es para recuperar un archivo que ha sido borrado"
+mv "$archivorec" "$directorio" 
+echo "Archivo recuperado"
+ls /home/amacalli/Descargas
